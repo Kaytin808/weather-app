e27dfe723267d6941f32c158a348b74ab3e0c3a6
Justin Moore <kaytin143@gmail.com>
2022-09-08 22:45:22 -0700

    Added gitignore

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const tempDiv = document.querySelector(".temp");
 const feelsLike = document.querySelector(".feelslike");
 const submitBtn = document.getElementById('btn')
 const searchField = document.getElementById('search')
+const span = document.createElement('span')
+const timezoneDiv = document.querySelector('.timezone')
+
 // For Celc conversion
 // http://api.openweathermap.org/data/2.5/weather?q=gilbert&units=metric&appid=beb6cbea1cce879189e3f8a1539625dc
 
@@ -15,11 +18,22 @@ async function getWeatherData(name) {
   const formData = await fetchData.json();
   const feelsLikeData = formData.main["feels_like"];
   const tempData = formData.main["temp"];
+  const weatherData = formData.weather[0]["description"]
+const test = formData.timezone;
+
+
+
+  // Uppercase the first letter 
+  const weatherCap = weatherData.charAt(0).toUpperCase() + weatherData.slice(1)
   nameDiv.textContent = formData.name;
-  weatherDiv.textContent = formData.weather[0]["description"];
+  weatherDiv.textContent = weatherCap;
   tempDiv.textContent = Math.trunc(tempData);
-  feelsLike.textContent = "Feel's like " + Math.trunc(feelsLikeData);
-  console.log(formData.main);
+  // Removing the decimal
+  feelsLike.textContent = "Feel's like " + Math.trunc(feelsLikeData) + "°F";
+  span.textContent = "°F"
+  console.log(test);
+  console.log(formData)
+  tempDiv.appendChild(span)
 }
 
 // Search by city name
@@ -28,4 +42,4 @@ console.log(searchField.value)
 getWeatherData(searchField.value)
 })
 
-getWeatherData('gilbert')
+// getWeatherData('gilbert')
